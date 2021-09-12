cbbed9368ec2c0c59733121f6bbf5b843cd8cfd1
sokol549 <sokol549@gmail.com>
2021-09-12 19:45:31 +0300

    Добавление бд

--- webapp/config.py
+++ webapp/config.py
@@ -1,2 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 WEATHER_DEFAULT_CITY = "London"
 WEATHER_API_KEY = "e781992e8f1f4b0fbcb95744210809" 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'..','webapp.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from webapp.model import db, News
 
 
 def get_html(url):
@@ -21,11 +23,21 @@ def get_python_news():
             title = news.text
             url= news['href']
             published = news.findNext('time').text
+            try:
+                published = datetime.strptime(published,'%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
             result_news.append({
                 'title':title,
                 'url':url,
                 'published':published
             })
-        return result_news
+            save_news(title, url, published)
     return False
 
+def save_news(title, url, published):
+    news_exist = News.query.filter(News.url == url).count()
+    if not news_exist:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
