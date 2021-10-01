1509db4a8922dbe5c7df26fc92b295e750f26020
sokol549 <sokol549@gmail.com>
2021-10-01 09:43:48 +0300

    Добавление Blueprints

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, current_user, login_required
 from requests.api import post
 from webapp.weather import weather_by_city
-from webapp.model import db, News
-from webapp.forms import LoginForm
+from webapp.model import db
+from webapp.user.models import User
+from webapp.user.forms import LoginForm
+from webapp.user.views import blueprint as user_blueprint
+from webapp.admin.views import blueprint as admin_blueprint
+from webapp.news.views import blueprint as news_blueprint
 
 
 def create_app():
@@ -11,40 +15,15 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    
-    @app.route('/')
-    def index():
-        title = "Новости Python"
-        weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
-        news_list = News.query.order_by(News.published.desc()).all()
-        return render_template('index.html', page_title=title,
-        weather=weather, news_list=news_list)
-
-    @app.route('/login')
-    def login():
-        title = "Авторизация"
-        login_form = LoginForm()
-        return render_template('login.html', page_title=title, form=login_form)
-    
-    @app.route('/process-login', methods=['POST'])
-    def process_login():
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = User.query.filter_by(username=form.username.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user)
-                flash('Вы вошли на сайт')
-                return redirect(url_for('index'))
-            flash("Неправильное имя пользователя или пароль")
-            return redirect(url_for(login))
-
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
     
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(news_blueprint)
+    app.register_blueprint(user_blueprint)
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(user_id)
-
     return app
-    
--- webapp/config.py
+++ webapp/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -8,3 +9,4 @@ WEATHER_API_KEY = "e781992e8f1f4b0fbcb95744210809"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'..','webapp.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'KLJ4H25KLJH34J5H34J5*&^%T#$Hb'
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
