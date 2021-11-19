781c93e0266243c8dfe481403468a8efc537844f
sokol <sokol549@gmail.com>
2021-11-19 13:08:03 +0300

    долго не делал коммитов

--- webapp/user/models.py
+++ webapp/user/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
     role = db.Column(db.String(10), index=True)
+    email = db.Column(db.String(50), unique=True)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -17,3 +18,6 @@ class User(db.Model, UserMixin):
         
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
