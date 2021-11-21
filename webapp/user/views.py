73f323f9fcba7a1eac4a2bba67457fd803ec0497
sokol <sokol549@gmail.com>
2021-11-21 16:54:31 +0300

    add button to main page but it working not good

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -23,7 +23,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user)
             flash('Вы вошли на сайт')
-            return redirect(get_redirect_target())
+            return redirect(news.index)
     flash("Неправильное имя пользователя или пароль")
     return redirect(url_for('user.login'))
 
